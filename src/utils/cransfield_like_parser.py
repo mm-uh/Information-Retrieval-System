6d9ce6ca407ae2753304c0e5ef1d9b33b2582454
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-05-06 21:21:31 -0400

    Make CransfieldParser a static class:

--- src/utils/cransfield_like_parser.py
+++ src/utils/cransfield_like_parser.py
@@ -1,22 +1,23 @@
-from typing import List
+from typing import List, Dict
+from collections import defaultdict
 from ..data.collection import Document
 import re
 
 SEPARATORS = ['.T', '.A', '.B', '.W', '.X']
 
+
 class CransfieldLikeParser:
-    def __init__(self):
-        pass
-    
-    def parse(self, file):
+    @staticmethod
+    def parse(file):
         with open(file, 'r') as f:
             file_data = ''.join(f.readlines())
         documents: List[str] = re.split('.I \d', file_data)
-        documents = list(map(lambda x : x.strip(), documents))
-        documents = list(filter(lambda x : x!='', documents))
-        return list(map(lambda x : self.__parse_document(x), documents))
-    
-    def __parse_document(self, document):
+        documents = [document_str.strip() for document_str in documents]
+        documents = [document_str for document_str in documents if not document_str == '']
+        return [CransfieldLikeParser.__parse_document(document) for document in documents]
+
+    @staticmethod
+    def __parse_document(document):
         document_lines: List[str] = document.split('\n')
         document_lines = list(map(lambda x: x.strip(), document_lines))
         available_separators: List[(int, str)] = []
@@ -26,9 +27,10 @@ class CransfieldLikeParser:
                 available_separators.append((index, sep))
             except ValueError:
                 pass
-        return self.__create_document(document_lines, available_separators)
-        
-    def __create_document(self, document_lines: List[str], sections):
+        return CransfieldLikeParser.__create_document(document_lines, available_separators)
+
+    @staticmethod
+    def __create_document(document_lines: List[str], sections):
         document: Document = Document()
         sections = sorted(sections)
         sections.append((len(document_lines), ''))
@@ -37,7 +39,7 @@ class CransfieldLikeParser:
                 break
             section_begin = index + 1
             section_end = sections[i+1][0]
-            data = ''.join(document_lines[section_begin : section_end])
+            data = ''.join(document_lines[section_begin: section_end])
             if header == '.T':
                 document.title = data
             elif header == '.A':
@@ -45,4 +47,14 @@ class CransfieldLikeParser:
             elif header == '.W':
                 document.abstract = data
         return document
-        
+
+    @staticmethod
+    def parse_relevances(file, treshold=5):
+        relations: Dict[int, List[int]] = defaultdict(lambda: list())
+        with open(file, 'r') as f:
+            for line in f.readlines():
+                query, document, relation, * \
+                    _ = map(lambda x: int(float(x)), line.split())
+                if relation <= treshold:
+                    relations[query - 1].append(document - 1)
+        return relations
