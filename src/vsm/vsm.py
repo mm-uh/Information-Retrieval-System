05f8fac64d84c28b8e20ee275970be63e5330787
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-05-08 18:38:05 -0400

    Add Rocchio algorithm to query method

--- src/vsm/vsm.py
+++ src/vsm/vsm.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, TypeVar
+from typing import List, Dict, TypeVar, Callable
 from ..index.index import Index
 from ..index.posting import Posting
 from ..utils.collection import Document, Collection
@@ -15,8 +15,10 @@ SMOOTH_VALUE = 0.5
 
 
 class VectorSpaceModel:
-    def __init__(self, collection: Collection, relevant_treshold: int = 10):
+    def __init__(self, collection: Collection, relevant_treshold: int = 10, alpha: float = 1, betha: float = 0.75):
         LOGGER.info("Initializing Model")
+        self.__alpha = alpha
+        self.__betha = betha
         self.__collection: Collection = collection
         LOGGER.info('Creating Index')
         self.__index: Index = Index(collection)
@@ -25,11 +27,12 @@ class VectorSpaceModel:
         self.__document_vectors: List[Vector] = [self.__create_document_vector(
             index) for index in range(len(self.__collection))]
 
-    def query(self, query: str) -> List[int]:
+    def query(self, query: str, relevants: List[int] = [], irrelevants: List[int] = []) -> List[int]:
         LOGGER.info(f"Querying {query}")
         scores: List[(float, int)] = []
+        query_vector: Vector = self.__create_query_vector(query, relevants, irrelevants)
         for index in range(len(self.__collection)):
-            score: float = self.__get_score(query, index)
+            score: float = self.__get_score(query_vector, index)
             scores.append((score, index))
 
         LOGGER.debug("Selecting best socores")
@@ -39,22 +42,15 @@ class VectorSpaceModel:
             indexes.append(sorted_scores[i][1])
         return indexes
 
-    def __get_score(self, query: str, document_index: int) -> float:
+    def __get_score(self, query_vector: Vector, document_index: int) -> float:
         LOGGER.debug("Computing score")
         document_vector: Vector = self.__document_vectors[document_index]
         document_vector = self.__normalize_vector(document_vector)
-        query_vector: Vector = self.__create_query_vector(query)
+        # query_vector: Vector = self.__create_query_vector(query, relevants)
         return self.__compute_similarity(query_vector, document_vector)
 
     def __compute_similarity(self, vector1: Vector, vector2: Vector) -> float:
-        similarity: float = 0.0
-        for word, weight1 in vector1.items():
-            try:
-                weight2: float = vector2[word]
-                similarity += weight1 * weight2
-            except KeyError:
-                continue
-        return similarity
+        return dot_product(vector1, vector2)
 
     def __get_weight(self, word: str, document_index: int) -> float:
         idf: float = self.__get_idf(word)
@@ -81,20 +77,48 @@ class VectorSpaceModel:
         norm: float = sqrt(sum(map(lambda x: x**2, weights)))
         return {word: (weight / norm) for word, weight in vector.items()}
 
-    def __create_query_vector(self, query: str) -> Vector:
+    def __create_query_vector(self, query: str, relevants: List[int], irrelevants: List[int]) -> Vector:
         LOGGER.debug("Creating query vector")
+
         query_words: List[str] = preprocess_data(query)
         query_words = self.__index.filter_non_indexed_words(query_words)
         frequencies: Counter = Counter(query_words)
         max_frequency = max(frequencies.values())
-        return {
-            word: self.__compute_query_weight(word, frequencies[word], max_frequency) for word in query_words}
+        alpha: float = 1 #if len(relevants) == 0 else self.__alpha
+        query_vector = {word: self.__compute_query_weight(
+            word, frequencies[word], max_frequency, alpha) for word in query_words}
+        print(sum(query_vector.values()))
+        relevant_weights = self.__compute_relevant_bias(relevants)
+        irrelevants_weights = self.__compute_irrelevant_bias(irrelevants)
+        result = vector_sum(query_vector, relevant_weights)
+        print(sum(result.values()))
+        result = vector_dif(result, irrelevants_weights)
+        print(sum(result.values()))
+        return result         
+    
+    def __compute_relevant_bias(self, relevants: List[int]) -> Vector:
+        relevant_documents = [self.__document_vectors[index] for index in relevants]
+        relevant_weights = {}
+        for rd in relevant_documents:
+            relevant_weights = vector_sum(relevant_weights, rd)
+        relevant_weights = apply_all_vector(relevant_weights, lambda x : x/len(relevant_weights))
+        relevant_weights = apply_all_vector(relevant_weights, lambda x : x*0.9)
+        return relevant_weights
+    
+    def __compute_irrelevant_bias(self, irrelevants: List[int]) -> Vector:
+        irrelevants_documents = [self.__document_vectors[index] for index in irrelevants]
+        irrelevants_weights = {}
+        for nr in irrelevants_documents:
+            irrelevants_weights = vector_sum(irrelevants_weights, nr)
+        irrelevants_weights = apply_all_vector(irrelevants_weights, lambda x : x/len(irrelevants_documents))
+        irrelevants_weights = apply_all_vector(irrelevants_weights, lambda x : x*0.3)
+        return irrelevants_weights
 
-    def __compute_query_weight(self, word: str, frequency: float, max_frequency: float) -> float:
+    def __compute_query_weight(self, word: str, frequency: float, max_frequency: float, alpha: float) -> float:
         tf: float = (SMOOTH_VALUE + (1 - SMOOTH_VALUE)
                      * frequency) / max_frequency
         idf: float = self.__get_idf(word)
-        return tf * idf
+        return tf * idf * alpha
 
     def __create_document_vector(self, document_index: int) -> Vector:
         LOGGER.debug("Creating document vector")
@@ -109,6 +133,47 @@ def save_model(model, filename):
     LOGGER.info(f'Saving model to file {filename}')
     pickle.dump(model, open(filename, 'wb'))
 
+
 def load_model(filename):
     LOGGER.info(f'Loading model from file {filename}')
     return pickle.load(open(filename, 'rb'))
+
+
+def vector_sum(vector1: Vector, vector2: Vector) -> Vector:
+    result: Vector = {}
+    for key, value in vector1.items():
+        try:
+            result[key] += value
+        except KeyError:
+            result[key] = value
+    for key, value in vector2.items():
+        try:
+            result[key] += value
+        except KeyError:
+            result[key] = value
+    return result
+
+def vector_dif(vector1: Vector, vector2: Vector) -> Vector:
+    result: Vector = {}
+    for key, value in vector1.items():
+        try:
+            result[key] = value - vector2[key]
+        except KeyError:
+            result[key] = value
+    return result
+
+def dot_product(vector1: Vector, vector2: Vector) -> float:
+    similarity: float = 0.0
+    for word, weight1 in vector1.items():
+        try:
+            weight2: float = vector2[word]
+            similarity += weight1 * weight2
+        except KeyError:
+            continue
+    return similarity
+
+def apply_all_vector(vector: Vector, f: Callable[[float], float]) -> Vector:
+    result: Vector = {}
+    for key, value in vector.items():
+        result[key] = f(value)
+    return result
